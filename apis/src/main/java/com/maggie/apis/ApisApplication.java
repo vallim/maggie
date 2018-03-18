512ef3781135f43fac8817e82a0b976456ef7572
Fabricio Valliim <fmvallim@gmail.com>
2018-03-17 23:30:22 -0300

    Creating a spring boot project to hold the rest apis

---
+++ apis/src/main/java/com/maggie/apis/ApisApplication.java
@@ -0,0 +1,12 @@
+package com.maggie.apis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApisApplication.class, args);
+	}
+}
