1d9d9854d134a747326b337ff948a1243b755f48
Fabricio Valliim <fmvallim@gmail.com>
2018-03-19 23:13:06 -0300

    Refactoring tests to use WebMockMvc since this way the execution is faster because is not necessary to start the server

--- apis/src/test/java/com/maggie/controller/StudentControllerIT.java
+++ apis/src/test/java/com/maggie/controller/StudentControllerIT.java
@@ -1,44 +1,56 @@
 package com.maggie.controller;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.maggie.model.Gender;
 import com.maggie.model.Student;
 import com.maggie.service.StudentService;
 import java.time.LocalDate;
+import java.util.Optional;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebMvcTest(StudentController.class)
 public class StudentControllerIT {
 
     @Autowired
-    StudentController studentController;
+    private MockMvc mockMvc;
 
-    @Autowired
+    @MockBean
     StudentService studentService;
 
     @Test
-    public void shouldFindAStudentByItsId() {
+    public void shouldFindAStudentByItsId() throws Exception {
 
         final Student john = Student.builder().id(1L).name("John").gender(Gender.MALE)
             .dateOfBirth(LocalDate.of(2001, 2, 10)).build();
-        studentService.save(john);
 
-        ResponseEntity responseEntity = studentController.findById(1L);
+        given(studentService.findById(1L)).willReturn(Optional.of(john));
 
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(200);
+        mockMvc.perform(get("/students/1"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.name", is(john.getName())))
+            .andExpect(jsonPath("$.gender", is(john.getGender().toString())))
+            .andExpect(jsonPath("$.dateOfBirth", is(john.getDateOfBirth().toString())));
     }
 
     @Test
-    public void shouldNotFindAStudentGivenAnInvalidId() {
-        ResponseEntity responseEntity = studentController.findById(1L);
+    public void shouldNotFindAStudentGivenAnInvalidId() throws Exception {
 
-        assertThat(responseEntity.getStatusCodeValue()).isEqualTo(404);
+        given(studentService.findById(1L)).willReturn(Optional.empty());
+
+        mockMvc.perform(get("/students/1"))
+            .andExpect(status().isNotFound());
     }
 }
