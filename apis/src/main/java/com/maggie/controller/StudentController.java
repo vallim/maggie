4962f733087fb54ddf4a9b1dc458f16111f9eb11
Fabricio Valliim <fmvallim@gmail.com>
2018-03-20 00:08:45 -0300

    Creating api to save a student

--- apis/src/main/java/com/maggie/controller/StudentController.java
+++ apis/src/main/java/com/maggie/controller/StudentController.java
@@ -2,13 +2,15 @@ package com.maggie.controller;
 
 import com.maggie.model.Student;
 import com.maggie.service.StudentService;
+import java.util.Optional;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/students")
@@ -25,5 +27,10 @@ public class StudentController {
         Optional<Student> student = studentService.findById(id);
 
         return student.isPresent() ? ResponseEntity.ok(student.get()) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody Student student) {
+        return new ResponseEntity(studentService.save(student), HttpStatus.CREATED);
     }
 }
